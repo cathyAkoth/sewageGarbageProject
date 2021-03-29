e937a6eca2ad6b46f6ced861b5c10ea0ab816193
cathyAkoth <78339848+cathyAkoth@users.noreply.github.com>
2021-03-29 14:59:12 -0700

    Added update and delete

--- employeeRoute.js
+++ employeeRoute.js
@@ -46,7 +46,42 @@ router.post('/employeeReg', upload.single('image'), async(req, res) => {
       
     });
 
-
+    //update record based on the _id from the database
+    // after adding the update button on the  employeelist
+    // renders the updateEmployee pug file where you can update
+    // add the `app.use('/public/images', express.static(__dirname + '/public/images'));`
+    // to index.js to make sure image is showing
+    router.get('/update/:id', async (req, res) => {
+        try {
+            const updateEmp = await Employee.findOne({ _id: req.params.id })
+            res.render('updateEmployee', { user: updateEmp })
+        } catch (err) {
+            res.status(400).send("Unable to find item in the database");
+        }
+    })
+    
+    // route to save the updated ddata
+    router.post('/update', async (req, res) => {
+        try {
+            await Employee.findOneAndUpdate({_id:req.query.id}, req.body)
+            res.redirect('/employee');
+        } catch (err) {
+            res.status(404).send("Unable to update item in the database");
+        }
+    })
+    
+    //delete and employee record from the database
+    // add the delete code to the employeelist pug file
+    router.post('/delete', async (req, res) => {
+        try {
+            await Employee.deleteOne({ _id: req.body.id })
+            res.redirect('back')
+        } catch (err) {
+            res.status(400).send("Unable to delete item in the database");
+        }
+    })
+    
+   
 
 
 
